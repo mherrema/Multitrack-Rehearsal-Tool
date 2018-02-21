a4f7840f0976134cabc25f6888ff99e1135297e6
Mitch Herrema <mherrema@yahoo.com>
2018-02-20 22:59:54 -0500

    upped the limit of songs displayed on the songs page

--- routes/views/songs.js
+++ routes/views/songs.js
@@ -59,7 +59,7 @@ exports = module.exports = function (req, res) {
 
 		var q = keystone.list('Song').paginate({
 			page: req.query.page || 1,
-			perPage: 10,
+			perPage: 30,
 			maxPages: 10,
 			filters: {
 				state: 'published',
